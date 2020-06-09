1fbd0ec66db1a16d52a44ce4bea12e4347cddad3
Nils Kober <nils.kober@students.uni-freiburg.de>
2020-06-09 16:21:32 +0200

    add my_ifft() and calc_T(), not yet tested

---
+++ Blatt 3/B0301.m
@@ -0,0 +1,3 @@
+function B0301()
+    
+end
---
+++ Blatt 3/my_fft.m
@@ -0,0 +1,43 @@
+function my_fft()
+    
+end
+
+% n = 2^s is the size of T and b(eta)
+function y = my_ifft(b, s)
+    n = 2^s;
+    T = calc_T(s);
+    if (s == 0)
+        return T*b;
+    end
+    s_new = s - 1;
+    n_new = 2^s_new;
+    % b_odd and b_even are interchanged in comparison to
+    % the lecture because indexing starts at 1 in matlab
+    b_even = zeros(n_new, 1);
+    b_odd = zeros(n_new, 1);
+    for i=1:n_new
+        b_even(i) = b(2*i);
+        b_odd(i) = b(2*i - 1);
+    end
+    D = zeros(n_new);
+    for i=1:n_new
+        D(i, i) = T(i, i);
+    end
+    res_1 = my_ifft(b_odd, s_new);
+    res_2 = D*my_ifft(b_even, s_new);
+    y_1 = res_1 + res_2;
+    y_2 = res_1 - res_2;
+    return [y_1; y_2];
+end
+
+function T = calc_T(s)
+    n = 2^s;
+    x = linspace(0,2*pi, n);
+    T = zeros(n);
+    for k=1:n
+        for l=1:n
+            exponent = 1i*k*x(l);
+            A(l, k) = exp(exponent);
+        end
+    end
+end
