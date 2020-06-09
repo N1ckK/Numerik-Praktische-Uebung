e65faaf063d9aa1a6e12cd607f3ed670a4b5acac
Nils Kober <nils.kober@students.uni-freiburg.de>
2020-06-09 17:58:52 +0200

    add plots to B0301.m, fft not yet working correctly

--- Blatt 3/B0301.m
+++ Blatt 3/B0301.m
@@ -5,8 +5,41 @@ function B0301()
             y = zeros(n,1);
             for i=0:n-1
                 y(i+1) = f(2*pi*i/n, r)
-                b = my_fft(y)
             end
+            b = my_fft(y)
+            b_ = fft(y)
+            plot_f(r);
+            hold on;
+            plot_fourier(b);
+            pause;
+            hold off;
         end
     end
+end
+
+function y = evalute_trig_interpolation(b, x)
+    n = length(b);
+    y = 0;
+    for k=0:n-1
+        exponent = 1i*k*x;
+        y = y+ b(k+1)*exp(exponent);
+    end
+end
+
+function plot_f(r)
+    x = linspace(0, 2*pi, 1000);
+    y = zeros(1000);
+    for i=1:1000
+        y(i) = f(x(i), r);
+    end
+    plot(x,y);
+end
+
+function plot_fourier(b)
+    x = linspace(0, 2*pi, 1000);
+    y = zeros(1000);
+    for i=1:1000
+        y(i) = evalute_trig_interpolation(b, x(i));
+    end
+    plot(x,y);
 end
