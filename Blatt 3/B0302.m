1fedd22bd044f19f495bb189988693806e7acca7
Nick Seinsche <=>
2020-06-16 22:15:12 +0200

    finished blatt 3 aufgabe 2

--- Blatt 3/B0302.m
+++ Blatt 3/B0302.m
@@ -1,18 +1,19 @@
 function B0302()
     % number of support points
     n = 2 ^ 5;
-    
-    % plots f_2 with n support points
-    plot_fourier(1, n);
-    pause();
-    clf('reset')
-    plot_fourier(2, n);
-    pause();
-    clf('reset')
-    plot_fourier(3, n);
-    pause();
-    clf('reset')
-    plot_fourier(4, n);
+      
+    for i = 1:4
+        % beta coefficients
+        figure(1);
+        plot_fourier(i, n, 'b');
+        
+        % delta coefficients
+        figure(2);
+        plot_fourier(i, n, 'd');
+        
+        pause();
+        clf('reset')
+    end
 end
 
 function B = calc_beta_coeffs(i, n)
@@ -24,8 +25,19 @@ function B = calc_beta_coeffs(i, n)
     B = fft(y) / n;
 end
 
-function value = fourier_from_coefficients(B, n, x, part)
-    % Calculates the imaginary part value at x of the fourier polynom
+function D = calc_delta_coeffs(i, n)
+    % Calculates delta coefficients for the function f_i using
+    % n+1 eqidistant support points
+    % Returns these coefficients
+    B = calc_beta_coeffs(i, n);
+    % ----------------------------- | ---------------------------
+    % b_n/2 | b_n/2+1 | ... | b_n-1 | b_0 | b_1 | ... | b_n/2-1 
+    % ----------------------------- | ---------------------------
+    D = [B(n/2+1 : n), B(1 : n/2)];
+end
+
+function value = values_from_beta_coeffs(B, n, x, part)
+    % Calculates the value at x of the fourier polynom
     % if part == 'r' -> real part, if part == 'i' -> imag part
     value = 0;
     for j = 0:n-1
@@ -36,45 +48,64 @@ function value = fourier_from_coefficients(B, n, x, part)
     value = (part == 'r') .* real(value) + (part == 'i') .* imag(value);
 end
 
-function plot_fourier(i, n)
-    B = calc_beta_coeffs(i, n);
+function value = values_from_delta_coeffs(B, n, x, part)
+    % Calculates the value at x of the fourier polynom
+    % if part == 'r' -> real part, if part == 'i' -> imag part
+    value = 0;
+    for j = 0:n-1
+        value = value + B(j + 1) * exp(1i * (j - n / 2) * x);
+    end
+    
+    % conditional assignment
+    value = (part == 'r') .* real(value) + (part == 'i') .* imag(value);
+end
+
+function plot_fourier(i, n, c)
+    % plots with delta coeffs if c == 'd' and with beta coeffs if c == 'b'
+    B = ((c == 'b') .* calc_beta_coeffs(i, n) ...
+       + (c == 'd') .* calc_delta_coeffs(i, n));
     
     % support points
     supp = arrayfun(@(j) 2 * pi * j / n, 0:n-1);
     
     x = linspace(0, 2 * pi, 450);
-    y_real = arrayfun(@(z) fourier_from_coefficients(B, n, z, 'r'), x);
-    y_imag = arrayfun(@(z) fourier_from_coefficients(B, n, z, 'i'), x);
     
-    % plot real part
+    % choose values
+    if c == 'b'
+        y_real = arrayfun(@(z) values_from_beta_coeffs(B, n, z, 'r'), x);
+        y_imag = arrayfun(@(z) values_from_beta_coeffs(B, n, z, 'i'), x);
+    elseif c == 'd'
+        y_real = arrayfun(@(z) values_from_delta_coeffs(B, n, z, 'r'), x);
+        y_imag = arrayfun(@(z) values_from_delta_coeffs(B, n, z, 'i'), x);
+    end
+    
+    % --- plot real part
     subplot(3, 1, 1);
     plot(x, y_real)
-    hold on
-    plot(x, arrayfun(@(z) real(f(z, i)), x));
-    plot(supp, arrayfun(@(z) real(f(z, i)), supp), "ob");
     
+    hold on
+    plot(x, arrayfun(@(z) real(f(z, i)), x), '--');
+    plot(supp, arrayfun(@(z) real(f(z, i)), supp), "ob");
+
     xlim([0 2*pi]);
-    %ylim([-5 5]);
     title("real part")
     hold off
     
-    % plot imag part
+    % --- plot imag part
     subplot(3, 1, 2);
     plot(x, y_imag)
     hold on
-    plot(x, arrayfun(@(z) imag(f(z, i)), x));
+    plot(x, arrayfun(@(z) imag(f(z, i)), x), '--');
     plot(supp, arrayfun(@(z) imag(f(z, i)), supp), "ob");
-    
+
     xlim([0 2*pi]);
-    %ylim([-5 5]);
     title("imag part")
     hold off
     
     % plot spectrum
-    hold on
     subplot(3, 1, 3);
     plot(0:n-1, abs(B));
+    
     xlim([0 n-1]);
     title("spectrum")
-    hold off
 end
